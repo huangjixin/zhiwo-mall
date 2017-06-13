8816b7d4db1e106b9fccadc4cdcd21fc3fc3679c
黄记新 <635152644@qq.com>
2017-06-13 17:12:40 +0800

    添加生成列表树方法

--- zhiwo-mall/zhiwo-mall-interface/src/main/java/com/zwo/modules/mall/service/IPrCategoryService.java
+++ zhiwo-mall/zhiwo-mall-interface/src/main/java/com/zwo/modules/mall/service/IPrCategoryService.java
@@ -3,6 +3,9 @@
  */
 package com.zwo.modules.mall.service;
 
+import java.util.List;
+
+import com.zwo.modules.mall.domain.CategoryTree;
 import com.zwo.modules.mall.domain.PrCategory;
 import com.zwotech.modules.core.service.IBaseService;
 
@@ -11,5 +14,10 @@ import com.zwotech.modules.core.service.IBaseService;
  *
  */
 public interface IPrCategoryService extends IBaseService<PrCategory> {
-
+	/**
+	 * 根据parentId查询树结构列表数组。
+	 * @param parentId
+	 * @return
+	 */
+	List<CategoryTree> getTreeCategory(String parentId);
 }
