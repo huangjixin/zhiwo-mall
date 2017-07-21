faa5ada95bbba9d4840d2cf0f91d4d3de0a164c5
黄记新 <635152644@qq.com>
2017-07-21 17:09:39 +0800

    导入会员

---
+++ zhiwo-member/zhiwo-member-interface/src/main/java/com/zwo/modules/member/service/App.java
@@ -0,0 +1,13 @@
+package com.zwo.modules.member.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
