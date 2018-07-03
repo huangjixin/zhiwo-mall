499b5b436573fc3b7cb947cd56cc7819f6b85fae
simonqian <simonqian@outlook.com>
2018-07-03 10:55:47 +0800

    FetchUtils.js添加超时提示

--- FwdIris-rn/src/common/FetchUtils.js
+++ FwdIris-rn/src/common/FetchUtils.js
@@ -1,99 +1,133 @@
+let FETCH_TIMEOUT = 5000
 
+export async function Get ({ url, params, success, error }) {
+  console.log('*******************request start**********************')
+  console.log('url:' + url)
 
-export async function Get({url,params,success,error}){
-    console.log('*******************request start**********************');
-    console.log('url:' + url);
+  if (url == null) {
+    return
+  }
 
-    if(url==null){
-        return ;
-    }
-
-    if(params!=null && typeof(params) == 'object'){
-        let paramStr = '';
-        Object.keys(params).forEach(key =>{
-            paramStr += key + '=' + params[key] + '&';
-        });
-        paramStr = paramStr.substring(0,paramStr.length-1);
-
-        url += '?'+paramStr;
-
-        console.log('request url:' + url);
-    }
-
-    await fetch(url,{
-        method: 'GET',
+  if (params != null && typeof params === 'object') {
+    let paramStr = ''
+    Object.keys(params).forEach(key => {
+      paramStr += key + '=' + params[key] + '&'
     })
-        .then(response =>response.json())
-        .then(respData => {
-            console.log(respData);
-            if(respData.code!=null&&respData.code==0){
-                if(typeof error === 'function'){
-                    error(respData);
-                }
-                // alert(respData.msg);
-                return;
-            }
+    paramStr = paramStr.substring(0, paramStr.length - 1)
 
-            if(respData.code!=null
-                && respData.code==1
-                && typeof success === 'function'){
-                success(respData);
-            }
+    url += '?' + paramStr
 
-        }).catch((errResp) => {
-            if(typeof error === 'function'){
-                error(errResp);
-            }
-            // alert('network error!');
-            console.log(errResp.toString());
-        });
+    console.log('request url:' + url)
+  }
 
-    console.log('*******************request end**********************');
+  new Promise((resolve, reject) => {
+    var timeout = setTimeout(() => {
+      reject({ status: 408, message: 'Request timed out' })
+    }, FETCH_TIMEOUT)
+
+    fetch(url, {
+      method: 'GET'
+    })
+      .then(response => {
+        clearTimeout(timeout)
+        if (response && response.ok) return response.json()
+        else reject(new Error('Response error'))
+      })
+      .then(responseObject => {
+        resolve(responseObject)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+    .then(respData => {
+      console.log(respData)
+      if (respData.code != null && respData.code == 0) {
+        if (typeof error === 'function') {
+          error(respData)
+        }
+        alert(respData.msg)
+        return
+      }
+
+      if (respData.code != null && respData.code == 1 && typeof success === 'function') {
+        success(respData)
+      }
+    })
+    .catch(errResp => {
+      if (typeof error === 'function') {
+        error(errResp)
+      }
+      if (errResp.status === 408) {
+        alert(errResp.message)
+      } else {
+        alert('network error!')
+      }
+
+      console.log(errResp.message)
+    })
+
+  console.log('*******************request end**********************')
 }
 
+export async function Post ({ url, params, success, error }) {
+  console.log('*******************request start**********************')
+  console.log('url:' + url)
 
-export async function Post({url,params,success,error}){
-    console.log('*******************request start**********************');
-    console.log('url:' + url);
+  if (url == null) {
+    return
+  }
 
-    if(url==null){
-        return ;
-    }
+  new Promise((resolve, reject) => {
+    var timeout = setTimeout(() => {
+      reject({ status: 408, message: 'Request timed out' })
+    }, FETCH_TIMEOUT)
 
-    await fetch(url,{
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params)
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
     })
-        .then(response =>response.json())
-        .then(respData => {
-            console.log(respData);
-            if(respData.code!=null&&respData.code==0){
-                if(typeof error === 'function'){
-                    error(respData);
-                }
-                // alert(respData.msg);
-                return;
-            }
+      .then(response => {
+        clearTimeout(timeout)
+        if (response && response.ok) return response.json()
+        else reject(new Error('Response error'))
+      })
+      .then(responseObject => {
+        resolve(responseObject)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+    .then(respData => {
+      console.log(respData)
+      if (respData.code != null && respData.code == 0) {
+        if (typeof error === 'function') {
+          error(respData)
+        }
+        alert(respData.msg)
+        return
+      }
 
-            if(respData.code!=null
-                && respData.code==1
-                && typeof success === 'function'){
-                success(respData);
-            }
+      if (respData.code != null && respData.code == 1 && typeof success === 'function') {
+        success(respData)
+      }
+    })
+    .catch(errResp => {
+      if (typeof error === 'function') {
+        error(errResp)
+      }
+      if (errResp.status === 408) {
+        alert(errResp.message)
+      } else {
+        alert('network error!')
+      }
+      console.log(errResp.message)
+    })
 
-        }).catch((errResp) => {
-            if(typeof error === 'function'){
-                error(errResp);
-            }
-            // alert('network error!');
-            console.log(errResp.toString());
-        });
-
-    console.log('*******************request end**********************');
+  console.log('*******************request end**********************')
 }
-
-
